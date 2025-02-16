5702c224278159994c5bf8f446e131f409e3fed0
munjuhyeok <munjuhyeok@gmail.com>
2025-02-16 22:47:14 +0900

    temp

--- src/raisin_bridge.cpp
+++ src/raisin_bridge.cpp
@@ -18,11 +18,44 @@
 #include "rclcpp/rclcpp.hpp"
 
 #include "raisin_network/network.hpp"
+#include "raisin_network/node.hpp"
+
+#include <ros2_raisin_interfaces/msg/imu.hpp>
 
 #include "interfaces.hpp"
 
 
+using namespace std::placeholders;  // To make _1, _2, etc., available
+
+class BridgeNode : public rclcpp::Node
+{
+  public:
+  BridgeNode()
+    : Node("minimal_subscriber")
+    {
+      subscription_ = this->create_subscription<ros2_raisin_interfaces::msg::Imu>(
+      "topic", 10, std::bind(&BridgeNode::topic_callback, this, _1));
+    }
+
+  private:
+    void topic_callback(const ros2_raisin_interfaces::msg::Imu msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+    }
+    rclcpp::Subscription<ros2_raisin_interfaces::msg::Imu>::SharedPtr subscription_;
+};
+
 int main(int argc, char * argv[])
 {
+    std::vector<std::vector<std::string>> threads = {{"main"}};
+    std::shared_ptr<raisin::Network> serverNetwork = std::make_shared<raisin::Network>(
+      "server",
+      "test",
+      threads);
+
+    raisin::Node node(serverNetwork);
+
+    auto publisher = node.createPublisher<raisin::raisin_interfaces::msg::Imu>("imu");
+
     return 0;
 }
