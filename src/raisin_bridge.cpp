34b068058490075b9314781a05a5acd29da53ce4
munjuhyeok <munjuhyeok@gmail.com>
2025-02-16 21:43:36 +0900

    feat: build suceed

---
+++ src/raisin_bridge.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) 2024 Raion Robotics, Inc.
+//
+// Any unauthorized copying, alteration, distribution, transmission,
+// performance, display or use of this material is prohibited.
+//
+// All rights reserved.
+
+//
+// Created by munjuhyeok on 2/16/24.
+//
+
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "raisin_network/network.hpp"
+
+#include "interfaces.hpp"
+
+
+int main(int argc, char * argv[])
+{
+    return 0;
+}
