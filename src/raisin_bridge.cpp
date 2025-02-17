72e668e60e039d098bef6f40edd388ceab546da1
munjuhyeok <munjuhyeok@gmail.com>
2025-02-17 17:13:32 +0900

    feat: tested imu, need generalization

--- src/raisin_bridge.cpp
+++ src/raisin_bridge.cpp
@@ -45,22 +45,52 @@ class BridgeNode : public rclcpp::Node
   private:
     void topic_callback(const raisin_interfaces::msg::Imu::SharedPtr msg) const
     {
-        msg->quaternion_w;
+      auto raisin_msg = to_raisin_msg(*msg);
+      std::cout<<"Received message: "<<raisin_msg.quaternion_w<<std::endl;
     }
     rclcpp::Subscription<raisin_interfaces::msg::Imu>::SharedPtr subscription_;
 };
 
+void subscriberCallback1(const std::shared_ptr<raisin::raisin_interfaces::msg::Imu> msg)
+{
+  std::cout << "subscriber1 works " << to_ros_msg(*msg).angular_velocity_x << std::endl;
+}
+
 int main(int argc, char * argv[])
 {
+  std::string serverId = "server";
+  std::string clientId = "client";
+
     std::vector<std::vector<std::string>> threads = {{"main"}};
-    std::shared_ptr<raisin::Network> serverNetwork = std::make_shared<raisin::Network>(
-      "server",
+    std::shared_ptr<raisin::Network> clientNetwork = std::make_shared<raisin::Network>(
+      clientId,
       "test",
       threads);
 
-    raisin::Node node(serverNetwork);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    // Attempt to connect to the server
+    std::shared_ptr<raisin::Remote::Connection> connection;
+    if (!connection) {
+      connection = clientNetwork->connect(serverId);
+      std::cerr << "[Client] Failed to connect to server at " << serverId << std::endl;
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    std::cout << "[Client] Connected to server at " << serverId << ". Starting message exchange..." <<
+      std::endl;
+
+    raisin::Node node(clientNetwork);
 
     auto publisher = node.createPublisher<raisin::raisin_interfaces::msg::Imu>("imu");
 
+    auto subscriber1 = node.createSubscriber<raisin::raisin_interfaces::msg::Imu>(
+      "imu", connection, std::bind(
+        subscriberCallback1,
+        _1));
+
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<BridgeNode>());
+    rclcpp::shutdown();
+    
     return 0;
 }
